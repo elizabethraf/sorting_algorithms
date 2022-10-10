6172f97aaff35047354f3080c4722e551e10d64d
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-10-10 15:02:36 -0700

    0-O

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,22 +8,4 @@
 **/
 void insertion_sort_list(listint_t **list)
 {
-	size_t a;
-	int key = 0;
-	size_t b;
-
-	for (a = 1; a < list; a++)
-	{
-		int key = a[b];
-		int b = a - 1;
-
-		while (b >= 0 && a[b] > key)
-		{
-			a[b + 1] = a[b];
-			b = b - 1;
-		}
-		a[b + 1] = key;
-	}
-}
-
-
+	listint_t *
